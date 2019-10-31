9eba46f84cb75b75a78706dc4223b3aac6e87a58
Ross McLane <ross.mclane@gmail.com>
2019-10-31 12:38:13 -0400

    Getting Mary Francis's Playlists using Spotify API

--- app.js
+++ app.js
@@ -7,8 +7,25 @@ $("#submit").on('click', function () {
     $.ajax({
         url: url,
         method: "GET"
-    }).done(function(response) {
-        console.log( $(response).rain );
-      });
+    }).done(function (response) {
+        const rain1h = $(response)[0].rain['1h'];
+
+        // Use your own token (this is just an example)
+        var token = 'BQA6K9PdygF5EipwKMKk1sANSe-JMO-eqKkLPdW3e5vb4DKfMTgWvlPxqSBzOV6YM4t-wX71VgO2-411xXF4GgNacwgAFMNt5PV6eXT9r-J2sy97SboSX42hOt3paKsjMV9qNKRidafEG2bFxo83PXm0-5vCABXeG4XqUh718Wj_Ymk4FQ'
+
+        $.ajax({
+            url: 'https://api.spotify.com/v1/users/mfskibiel/playlists/',
+            headers: {
+                Authorization: 'Bearer ' + token,
+            }
+        }).then(function (oData) {
+            const playlists = oData.items;
+            for (playlist of playlists){
+                console.log(playlist.name);
+            }
+        })
+
+
+    });
 
 });
