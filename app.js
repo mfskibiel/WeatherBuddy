dc879ec6be323f6dd7fa5b819c972642f20badf8
Ross McLane <ross.mclane@gmail.com>
2019-11-04 12:38:41 -0500

    Putting Open Weather Call into a function

--- app.js
+++ app.js
@@ -7,6 +7,39 @@ if (window.location.hash !== "") {
 
     localStorage.setItem("hash", accessToken);
 
+    openweatherCall();
+
+}
+
+$("#submit").on("click", function () {
+    let location = $("#location").val();
+    localStorage.setItem("city", location);
+    console.log(localStorage.getItem("city"));
+});
+
+function renderLink(link, linkname) {
+    var newdiv = $("<div>").html(`<a href=${link}>${linkname}</a>`);
+    $("body").append(newdiv);
+};
+
+function spotifyCall(weatherName) {
+    return $.ajax({
+        url: 'https://api.spotify.com/v1/users/mfskibiel/playlists/',
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem("hash"),
+        }
+    }).then(function (oData) {
+        const playlists = oData.items;
+        for (playlist of playlists) {
+            if (playlist.name === weatherName) {
+                var playlistLink = playlist.external_urls.spotify;
+                return playlistLink;
+            }
+        }
+    });
+};
+
+function openweatherCall(){
     // get weather condition
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${localStorage.getItem("city")}&apikey=78c2217a0f8204684461043d3c5ef215`;
 
@@ -53,31 +86,4 @@ if (window.location.hash !== "") {
         }
     });
 
-    function renderLink(link, linkname) {
-        var newdiv = $("<div>").html(`<a href=${link}>${linkname}</a>`);
-        $("body").append(newdiv);
-    };
-
-    function spotifyCall(weatherName) {
-        return $.ajax({
-            url: 'https://api.spotify.com/v1/users/mfskibiel/playlists/',
-            headers: {
-                Authorization: 'Bearer ' + localStorage.getItem("hash"),
-            }
-        }).then(function (oData) {
-            const playlists = oData.items;
-            for (playlist of playlists) {
-                if (playlist.name === weatherName) {
-                    var playlistLink = playlist.external_urls.spotify;
-                    return playlistLink;
-                }
-            }
-        });
-    };
 }
-
-$("#submit").on("click", function () {
-    let location = $("#location").val();
-    localStorage.setItem("city", location);
-    console.log(localStorage.getItem("city"));
-});
